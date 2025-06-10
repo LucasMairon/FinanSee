828f645a7c7212a12571dd2eb62b5a2dfe8abf7b
lidiana.souza <lidiana.souza@alunos.ufersa.edu.br>
2025-06-10 16:18:20 +0000

    feat: Adding logout functionality.

--- backend/core/config/api_routers.py
+++ backend/core/config/api_routers.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 
+from users.api.views import LogoutView
 
 urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.urls')),
+    path('auth/logout/', LogoutView.as_view(), name='logout')
 ]
