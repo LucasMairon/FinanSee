07a0253d76f3644f92c4b0bef45cf834738ad26a
cdanniel2002 <carlosdanniel2002.outlook.com>
2025-06-13 20:32:01 -0300

    feat: Adding category functionality.

---
+++ backend/core/categories/api/views.py
@@ -0,0 +1,17 @@
+from rest_framework.viewsets import ModelViewSet
+
+from rest_framework.permissions import IsAuthenticated
+
+from categories.models import Category
+
+from categories.api.permissions import IsOwner
+
+from categories.api.serializers import CategorySerializer
+
+
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Category.objects.filter(user=self.request.user)
--- backend/core/config/api_routers.py
+++ backend/core/config/api_routers.py
@@ -1,9 +1,17 @@
 from django.urls import include, path
 
+from rest_framework import routers
 from users.api.views import LogoutView
+
+from categories.api.views import CategoryViewSet
+
+router = routers.SimpleRouter()
+router.register('categories', CategoryViewSet, 'category')
 
 urlpatterns = [
     path('auth/', include('djoser.urls.jwt')),
     path('auth/', include('djoser.urls')),
     path('auth/logout/', LogoutView.as_view(), name='logout')
 ]
+
+urlpatterns += router.urls
