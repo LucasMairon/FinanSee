15036f7a385fcbd5a3a381505e0e890c9d035b14
lidianacosta <lidianacostasouza@gmail.com>
2025-07-02 08:27:43 -0300

    fix: fix: modifying conversion for production variables.

--- backend/core/config/settings/production.py
+++ backend/core/config/settings/production.py
@@ -3,6 +3,8 @@ from .base import *
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [
+                       s.strip() for s in v.split(',')])
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', cast=lambda v: [
+                    s.strip() for s in v.split(',')])
