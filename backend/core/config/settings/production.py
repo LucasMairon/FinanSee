c452f1c44084eaf4517c5e46892414d35d1481d8
lidianacosta <lidianacostasouza@gmail.com>
2025-07-21 23:38:08 -0300

    feat(config): making CORS_ALLOW_ALL_ORIGINS configurable via environment variable.

--- backend/core/config/settings/production.py
+++ backend/core/config/settings/production.py
@@ -1,10 +1,19 @@
 from .base import *
 
-
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [
-                       s.strip() for s in v.split(',')])
+CORS_ALLOW_ALL_ORIGINS = config('CORS_ALLOW_ALL_ORIGINS',
+                                cast=bool,
+                                default=False)
 
-SECRET_KEY = config('SECRET_KEY', cast=lambda v: [
-                    s.strip() for s in v.split(',')])
+if not CORS_ALLOW_ALL_ORIGINS:
+    CORS_ALLOWED_ORIGINS = config(
+        'CORS_ALLOWED_ORIGINS',
+        cast=lambda v: [s.strip()for s in v.split(',')],
+        default=[])
+
+SECRET_KEY = config('SECRET_KEY',
+                    cast=lambda v: [s.strip() for s in v.split(',')])
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS',
+                       cast=lambda v: [s.strip() for s in v.split(',')])
